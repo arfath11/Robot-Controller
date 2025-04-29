ee0fe9586f574c2a0f9b202b04c7322e0b1f0a08
arfath <md8arfath@gmail.com>
2025-04-29 19:22:08 +0530

    OpenGL version check

--- app/src/main/java/com/example/robotoperator/MainActivity.kt
+++ app/src/main/java/com/example/robotoperator/MainActivity.kt
@@ -1,9 +1,6 @@
 package com.example.robotoperator
 
-import android.app.ActivityManager
-import android.content.Context
 import android.os.Bundle
-import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
@@ -17,14 +14,8 @@ import androidx.compose.ui.tooling.preview.Preview
 import com.example.robotoperator.ui.theme.RobotOperatorTheme
 
 class MainActivity : ComponentActivity() {
-    private val TAG = "OpenGLCheck"
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        
-        // Check OpenGL ES version
-        checkOpenGLVersion()
-        
         enableEdgeToEdge()
         setContent {
             RobotOperatorTheme {
@@ -36,24 +27,6 @@ class MainActivity : ComponentActivity() {
                 }
             }
         }
-    }
-
-    private fun checkOpenGLVersion() {
-        val activityManager = getSystemService(Context.ACTIVITY_SERVICE) as ActivityManager
-        val configurationInfo = activityManager.deviceConfigurationInfo
-        
-        // Get the OpenGL ES version as a double
-        val openGLVersion = configurationInfo.glEsVersion.toDouble()
-        
-        Log.d(TAG, "Device OpenGL ES version: $openGLVersion")
-        
-        // Check if the device supports OpenGL ES 3.2
-        val supportsOpenGLES32 = openGLVersion >= 3.2
-        Log.d(TAG, "Supports OpenGL ES 3.2: $supportsOpenGLES32")
-        
-        // Get supported OpenGL extensions
-        val extensions = android.opengl.GLES32.glGetString(android.opengl.GLES32.GL_EXTENSIONS)
-        Log.d(TAG, "Supported OpenGL extensions: $extensions")
     }
 }
 
