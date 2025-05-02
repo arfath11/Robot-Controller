424ba76e9c0183eea4e39cd3708cfee950fe1100
arfath <md8arfath@gmail.com>
2025-05-02 17:37:40 +0530

    room, hilt, vm dependency added

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,6 +2,15 @@ plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
+
+    // Kotlin serialization plugin for type safe routes and navigation arguments
+    kotlin("plugin.serialization") version "2.0.0"
+    
+    // Use KSP plugin from the root build.gradle.kts
+    id("com.google.devtools.ksp")
+    
+    // Use Hilt plugin from the root build.gradle.kts
+    id("com.google.dagger.hilt.android")
 }
 
 android {
@@ -57,4 +66,17 @@ dependencies {
     androidTestImplementation(libs.androidx.ui.test.junit4)
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
+
+    // Viewmodel
+    implementation("androidx.lifecycle:lifecycle-viewmodel-compose")
+
+    // ROOM
+    implementation("androidx.room:room-runtime:${rootProject.extra["room_version"]}")
+    ksp("androidx.room:room-compiler:${rootProject.extra["room_version"]}")
+    implementation("androidx.room:room-ktx:${rootProject.extra["room_version"]}")
+
+    // Dragger hilt
+    implementation("com.google.dagger:hilt-android:2.48")
+    ksp("com.google.dagger:hilt-android-compiler:2.48")
+
 }
