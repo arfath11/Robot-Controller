5ac15cb0c326c766c12037e24e34ddcbe0dc2f23
arfath <md8arfath@gmail.com>
2025-05-02 17:37:40 +0530

    room, hilt, vm dependency added

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,4 +3,14 @@ plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.kotlin.android) apply false
     alias(libs.plugins.kotlin.compose) apply false
+
+    // Declare plugins at root level with apply false
+    id("com.google.dagger.hilt.android") version "2.48" apply false
+    id("com.google.devtools.ksp") version "2.0.21-1.0.27" apply false
+}
+
+buildscript {
+    extra.apply {
+        set("room_version", "2.6.1")
+    }
 }
